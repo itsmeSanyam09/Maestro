ed112deba08641929b4463236a2980881964a42e
itsmeSanyam09 <sanyambothra6@gmail.com>
2026-01-11 20:46:26 +0530

    Update next.config.ts to enhance server actions
    Increase body size limit for server actions to 30MB.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "30mb", // Increase this to 10MB or more
+    },
+  },
 };
 
 export default nextConfig;
